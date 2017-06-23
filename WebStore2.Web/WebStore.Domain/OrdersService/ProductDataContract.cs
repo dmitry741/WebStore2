0e02ee16c6787c1ad9c2ec5e62bffb72a758efa0
dmitry741 <dmitrypavlov74@gmail.com>
2017-06-23 15:58:11 +0300

    project template initial commit
    ridan, new computer

---
+++ WebStore2.Web/WebStore.Domain/OrdersService/ProductDataContract.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore2.Domain.OrdersService
+{
+    // TODO: add code later
+    //class ProductDataContract
+    //{
+    //}
+}
