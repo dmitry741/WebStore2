d5633af7e21d0d7333f3ed7066cd6246a7f5ea35
dmitry741 <dmitrypavlov74@gmail.com>
2017-06-25 12:18:54 +0300

    add First migration and update database
    home

---
+++ WebStore2.Web/WebStore2.DAL/Migrations/201706250908221_First.cs
@@ -0,0 +1,28 @@
+namespace WebStore2.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class First : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 250),
+                        Price = c.Int(nullable: false),
+                        Category = c.String(nullable: false, maxLength: 250),
+                    })
+                .PrimaryKey(t => t.id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+        }
+    }
+}
